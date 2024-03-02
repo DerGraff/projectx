7a76d5f15ce8a48645d1cf421543f6f832a00bc0
Rene Graff <r.graff@walbert-schmitz.de>
2024-03-02 22:41:18 +0100

    Refactor installation script and add conditional directory creation

--- install/installServices.sh
+++ install/installServices.sh
@@ -5,7 +5,23 @@ sudo apt-get -y install cec-utils
 # currentuser=$(whoami)
 current_user="pi"
 echo "Installing Dirs"
-sudo mkdir /progs && sudo chown -R $currentuser:$currentuser /progs/ && mkdir /progs/nodejs && mkdir /progs/nodejs/nodelocal && mkdir /progs/quasar
+
+if [ ! -d "/progs" ]; then
+    sudo mkdir /progs
+    sudo chown -R $currentuser:$currentuser /progs/
+fi
+
+if [ ! -d "/progs/nodejs" ]; then
+    mkdir /progs/nodejs
+fi
+
+if [ ! -d "/progs/nodejs/nodelocal" ]; then
+    mkdir /progs/nodejs/nodelocal
+fi
+
+if [ ! -d "/progs/quasar" ]; then
+    mkdir /progs/quasar
+fi
 
 echo "Installing ClientService"
 git -C /progs/nodejs/nodelocal clone https://ws-hub.de/node/libs/clientservice.git
@@ -14,12 +30,28 @@ npm link --prefix /progs/nodejs/nodelocal/clientservice
 
 
 echo "Installing VideoApp"
+
+if [ ! -d "/progs/quasar/videoApp" ]; then
+    echo "Cloning ClientLinuxService"
+    git -C /progs/quasar/ clone https://ws-hub.de/quasar/videoApp.git
+fi
+
+cd /progs/quasar/videoApp && npm ci && quasar build -m electron
+
+echo "Installing ClientLinuxService"
+git -C /progs/nodejs/ clone https://ws-hub.de/node/apps/clientlinuxservice.git && cd /progs/nodejs/clientlinuxservice/ && npm install
+# sudo ln -s "$NVM_DIR/versions/node/$(nvm version)/lib/node_modules/npm" "/usr/local/bin/npm"
+# sudo ln -s "$NVM_DIR/versions/node/$(nvm version)/bin/node" "/usr/local/bin/node"
+cd /progs/nodejs/clientlinuxservice/ && sudo node install.js
+
+sudo adduser ws dialout && sudo service clientservice start && sudo systemctl enable clientservice.service
+ 
 git -C /progs/quasar/ clone https://ws-hub.de/quasar/videoApp.git && cd /progs/quasar/videoApp && npm ci && quasar build -m electron
 
 echo "Installing ClientLinuxService"
 git -C /progs/nodejs/ clone https://ws-hub.de/node/apps/clientlinuxservice.git && cd /progs/nodejs/clientlinuxservice/ && npm install
-sudo ln -s "$NVM_DIR/versions/node/$(nvm version)/lib/node_modules/npm" "/usr/local/bin/npm"
-sudo ln -s "$NVM_DIR/versions/node/$(nvm version)/bin/node" "/usr/local/bin/node"
+# sudo ln -s "$NVM_DIR/versions/node/$(nvm version)/lib/node_modules/npm" "/usr/local/bin/npm"
+# sudo ln -s "$NVM_DIR/versions/node/$(nvm version)/bin/node" "/usr/local/bin/node"
 cd /progs/nodejs/clientlinuxservice/ && sudo node install.js
 
 sudo adduser ws dialout && sudo service clientservice start && sudo systemctl enable clientservice.service
